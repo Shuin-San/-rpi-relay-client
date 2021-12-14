6ef75f2b3bb5b71196b203d2dc0eebbfa64b0bf9
Zalimidis Vasileios <zalimidis@gmail.com>
2021-12-14 22:49:12 +0100

    Setup for work

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React, { useState } from "react";
-import ClientComponent from "./client/ioClient";
-import {Container, Grid} from "@material-ui/core";
+import React, { useState } from 'react'
+import ClientComponent from './client/ioClient'
+import { Container, Grid } from '@material-ui/core'
 
 function App() {
   return (
-      <Container maxWidth='lg' style={{minHeight: '100vh', backgroundColor: '#8d99ae', fontFamily : "Roboto"}}>
-          <div style={{paddingTop : 10}}>
-              <h1>Raspberry Pi Relay IOT Controller</h1>
-          </div>
-          <ClientComponent/>
-      </Container>
-  );
+    <Container
+      maxWidth='lg'
+      style={{
+        minHeight: '100vh',
+        backgroundColor: '#8d99ae',
+        fontFamily: 'Roboto',
+      }}
+    >
+      <div style={{ paddingTop: 10 }}>
+        <h1>Raspberry Pi Garden waterer</h1>
+      </div>
+      <ClientComponent />
+    </Container>
+  )
 }
 
-export default App;
+export default App
--- src/client/ioClient.js
+++ src/client/ioClient.js
@@ -1,42 +1,46 @@
-import React, { useEffect, useState } from "react";
-import {Grid, CircularProgress} from '@material-ui/core';
-import socketIOClient from "socket.io-client";
-import PumpComponent from "./pumpStatus/PumpComponent";
-const ENDPOINT = "http://192.168.0.100:4001";
+import React, { useEffect, useState } from 'react'
+import { Grid, CircularProgress } from '@material-ui/core'
+import socketIOClient from 'socket.io-client'
+import PumpComponent from './pumpStatus/PumpComponent'
+const ENDPOINT = 'http://shuinland.servebeer.com:4001'
 
 export default function ClientComponent(props) {
-    const [pumps, setPumps] = useState([]);
-    const [pin, setPin] = useState(false);
+  const [pumps, setPumps] = useState([])
+  const [pin, setPin] = useState(false)
 
-    const socket = socketIOClient(ENDPOINT);
+  const socket = socketIOClient(ENDPOINT)
 
-    function fetchCurrentStatus(){
+  function fetchCurrentStatus() {
+    socket.emit('getPumps', '')
+    socket.on('getPumps', (pumpData) => {
+      setPumps(pumpData)
+    })
+    // setTimeout(function () {
+    //     socket.disconnect()
+    // }, 5000, socket);
+  }
 
-        socket.emit('getPumps', '');
-        socket.on('getPumps', (pumpData) => {
-            setPumps(pumpData);
-        });
-        // setTimeout(function () {
-        //     socket.disconnect()
-        // }, 5000, socket);
-    }
+  useEffect(() => {
+    fetchCurrentStatus()
+    console.log(`Pumps Updated !`)
+  }, [])
 
-    useEffect(() => {
-        fetchCurrentStatus();
-        console.log(`Pumps Updated !`);
-        }, []);
-
-    return (
-            <Grid container>
-                {
-                    pumps && pumps.length > 0 ?
-                        pumps.map(
-                            ({id, pinStatus, runtime}) =>
-                                <PumpComponent id={id} pinStatus={pinStatus} setPumps={setPumps} pumps={pumps[id]} socket={socket} runtime={runtime}/>
-                            ) : <CircularProgress />
-
-                }
-
-            </Grid>
-    );
+  return (
+    <Grid container>
+      {pumps && pumps.length > 0 ? (
+        pumps.map(({ id, pinStatus, runtime }) => (
+          <PumpComponent
+            id={id}
+            pinStatus={pinStatus}
+            setPumps={setPumps}
+            pumps={pumps[id]}
+            socket={socket}
+            runtime={runtime}
+          />
+        ))
+      ) : (
+        <CircularProgress />
+      )}
+    </Grid>
+  )
 }
